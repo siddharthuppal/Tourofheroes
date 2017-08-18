da354ff607e02fb51c71d8bdff6b571088c68330
suppal <suppal>
2017-08-18 17:53:25 +0530

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import { TestcompComponent } from './testcomp/testcomp.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import 'rxjs/add/operator/switchMap';
     HeroDetailComponent,
     HeroesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    TestcompComponent
   ],
   imports: [
     BrowserModule,
